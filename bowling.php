2e1f186fe202b44130cebaa85430cf30f39ce257
Jacob Landry <jacoblandry@Jacobs-MacBook-Pro.local>
2022-03-25 22:34:39 -0400

    adding a json scorecard that we can use in other languages so we're always testing with the same data

--- bowling.php
+++ bowling.php
@@ -3,57 +3,32 @@
 require_once('Frame.php');
 require_once('ScoreCard.php');
 
-$perfectGame = [
-    [10],
-    [10],
-    [10],
-    [10],
-    [10],
-    [10],
-    [10],
-    [10],
-    [10],
-    [10, 10, 10],
-];
-testScores($perfectGame);
+// grab our scorecards that we'll use for testing all languages
+$games = json_decode(file_get_contents('games.json'));
 
-$spares = [
-    [8, 2], // 15
-    [5, 5], // 12
-    [2, 8], // 11
-    [1, 9], // 19
-    [9, 1], // 13
-    [3, 7], // 16
-    [6, 4], // 14
-    [4, 6], // 11
-    [1,9],  // 19
-    [9, 1, 5]   // 15
-    // 146
-];
-testScores($spares);
+// submit each scorecard and test against the score we know it should have
+foreach($games->scores as $game) {
+    $frames = $game->frames;
+    $score = $game->score;
 
-$random = [
-    [1, 4], // 5
-    [2, 6], // 8
-    [8, 0], // 8
-    [9, 0], // 9
-    [10],   // 13
-    [1, 2], // 3
-    [10],   // 28
-    [10],   // 20
-    [8, 2], // 15
-    [5, 5, 2]   // 12
-    // 121
-];
-testScores($random);
+    testScores($frames, $score);
+}
 
-function testScores($throws) {
+// validate scores
+function testScores($throws, $expected) {
     $frames = [];
     foreach($throws as $throw) {
         $frames[] = new Frame($throw);
     }
     $card = new ScoreCard($frames);
-    print("Score: " . $card->score . "\r\n");
+
+    print("Expected: " . $expected . "\n");
+    print("Score: " . $card->score . "\n");
+    if($expected == $card->score) {
+        print("Success!\n\n");
+    } else {
+        print("Failed!\n\n");
+    }
 }
 
 ?>
